7a7b797f77e6220cb929681078a7d509e15ac995
Anujeet Swain <anujeet98@gmail.com>
2024-05-03 03:41:20 +0530

    pop actions interval

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,23 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './grid.module.css';
 import Light from './Light';
 
 const Grid = () => {
-
-    const [stack, setStack] = useState([]);
+    const [stack, setStack] = useState(Array<Number>);
     const turnOnLight = (index: number) => {
-
-      if(stack.length!==9){
-        stack.push({});
-        setLightState(prev=>{
-            const updated = [...prev];
-            updated[row][col]=1;
-            return updated;
-        });
-      }
-
-      console.log(stack);
+		if(stack.includes(index)) return;
+		if(stack.length!==9){
+			setStack((oldStack)=>{
+				const updatedStack = [...oldStack];
+				updatedStack.push(index);
+				return updatedStack;
+			});
+		}
     }
+
+
+	useEffect(()=>{
+		let count=9;
+		function popFromStack(){
+			if(count-- > 0){
+				setStack(prevStack => prevStack.slice(0, -1));
+			}
+			else{
+				clearInterval(intervalid);
+			}
+		}
+		let intervalid = stack.length===9 ? setInterval(popFromStack, 300) : 0;
+	}, [stack]);
 
 	const lights = [];
 	for(let i=0; i<9; i++){
